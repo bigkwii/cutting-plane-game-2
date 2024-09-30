b0b8e2f6cf2dcbaf771caeff0824061eeb0298fd
big_kwii <alvaromoralest@gmail.com>
2024-09-30 17:29:51 -0300

    fixed minor visual bugs with split and circle cuts caused by aliassing

--- scenes/split_vfx.gd
+++ scenes/split_vfx.gd
@@ -2,7 +2,7 @@
 extends Node2D
 
 @export var width: float = 100
-@export var length: float = 1000
+@export var length: float = 2000
 @export var is_horizontal = false
 @export var growing_factor: float = 0 # from 0 to 1
 @export var color: Color = Color(1, 1, 1, 1)
@@ -20,11 +20,11 @@ func _process(_delta):
 
 func _draw():
 	if is_horizontal:
-		draw_line(Vector2(-length, growing_factor * width), Vector2(length, growing_factor * width), color, 1)
-		draw_line(Vector2(-length, -1 * growing_factor * width), Vector2(length, -1 * growing_factor * width), color, 1)
+		draw_line(Vector2(-length, growing_factor * width), Vector2(length, growing_factor * width), color, 1.5)
+		draw_line(Vector2(-length, -1 * growing_factor * width), Vector2(length, -1 * growing_factor * width), color, 1.5)
 	else:
-		draw_line(Vector2(growing_factor * width, -length), Vector2(growing_factor * width, length), color, 1)
-		draw_line(Vector2(-1 * growing_factor * width, -length), Vector2(-1 * growing_factor * width, length), color, 1)
+		draw_line(Vector2(growing_factor * width, -length), Vector2(growing_factor * width, length), color, 2)
+		draw_line(Vector2(-1 * growing_factor * width, -length), Vector2(-1 * growing_factor * width, length), color, 1.5)
 
 # play the grow animation
 func play_grow():
